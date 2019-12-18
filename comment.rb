278f9eb039db1c21db5ff0356b3ef96667442944
EC2 Default User <ec2-user@ip-172-31-45-11.ap-northeast-1.compute.internal>
2019-12-18 06:59:12 +0000

    新規ファイル作成

---
+++ comment.rb
@@ -0,0 +1,13 @@
+# puts 'Hello, world!'
+
+=begin
+puts 'Hello, world!'
+puts 'Hello, world!'
+puts 'Hello, world!'
+=end
+
+# puts 'Hello, world!'
+# puts 'Hello, world!'
+# puts 'Hello, world!'
+
+# This is a comment
