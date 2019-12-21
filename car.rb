8382a36f82911935c61cc890ba1893ebc62b3e1f
cyacyacho <kinpika8@gmail.com>
2019-12-21 06:24:40 +0000

    class演習

--- car.rb
+++ car.rb
@@ -1,5 +1,23 @@
 # Carクラスの作成
 # メソッド名はhello（helloと出力する機能をもつ）
 
-class "car"
-    car.hello puts "hello"
+# class "car"
+#     car.hello puts "hello"
+
+# 正解
+class Car
+    def initialize(name)
+        puts 'initialize'
+        @name = name
+    end
+    
+    def hello
+        puts "Hello! I am #{@name}."
+    end
+end
+
+car = Car.new('ESSE')
+car.hello
+
+karr = Car.new('Toppo')
+karr.hello
