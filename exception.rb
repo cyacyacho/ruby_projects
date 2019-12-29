c4b2758d8c9ef4a8a55998bb6b006a9f23f9c830
cyacyacho <kinpika8@gmail.com>
2019-12-29 05:10:14 +0000

    rubocopで修正

--- exception.rb
+++ exception.rb
@@ -16,15 +16,17 @@
 # i = gets.to_i
 
 # begin
-#     puts 10 / i
-#     puts "begin's end"
+#  puts 10 / i
+#  puts "begin's end"
 # rescue => ex
-#     puts "Error!"
-#     puts ex.message
-#     puts ex.class
+#  puts "Error!"
+#  puts ex.message
+#  puts ex.class
 # ensure
 #     puts "end"
 # end
+
+# 補足…例外が発生した場合、それ以降の処理は実行されない。
 
 # 復習
 
@@ -32,15 +34,14 @@ puts "--- Please enter an integer. ---"
 i = gets.to_i
 
 begin
-    puts 10 / i
+  puts 10 / i
 rescue => ex
-    puts "error!"
-    puts ex.class
-    puts ex.message
+  puts "error!"
+  puts ex.class
+  puts ex.message
 ensure
-    puts "end."
+  puts "end"
 end
-
 
 
 
