8c9b731d42a5a30a279d0a6cebc83d24b3b0a898
cyacyacho <kinpika8@gmail.com>
2019-12-29 04:53:01 +0000

    例外処理の演習

--- exception.rb
+++ exception.rb
@@ -2,3 +2,63 @@
 # １．数値１０をユーザーが入力した数値で割り、その結果を出力するプログラムを作成
 # ２．数値が０で割り当てられたときに、その例外をキャッチして
 # ３．エラーメッセージを出力する。
+
+# puts 10 / i
+
+# if i == 0
+#     puts "error!"
+# end
+
+# i(1)
+
+# 模範解答
+# puts "--- Please enter an integer. ---"
+# i = gets.to_i
+
+# begin
+#     puts 10 / i
+#     puts "begin's end"
+# rescue => ex
+#     puts "Error!"
+#     puts ex.message
+#     puts ex.class
+# ensure
+#     puts "end"
+# end
+
+# 復習
+
+puts "--- Please enter an integer. ---"
+i = gets.to_i
+
+begin
+    puts 10 / i
+rescue => ex
+    puts "error!"
+    puts ex.class
+    puts ex.message
+ensure
+    puts "end."
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
